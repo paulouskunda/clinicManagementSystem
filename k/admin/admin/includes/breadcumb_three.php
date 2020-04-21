8d2f89b9202dba6fcd42356a4b5c8314e4690971
Paulous <paulouskunda@gmail.com>
2020-04-21 11:23:35 +0200

    Remove K-folder

--- k/admin/admin/includes/breadcumb_three.php
+++
@@ -1,9 +0,0 @@
-<div id="content" class="span10">
-	<ul class="breadcrumb">
-		<li>
-			<i class="icon-home"></i>
-			<a href="index.php">Home</a> 
-			<i class="icon-angle-right"></i>
-		</li>
-		<li><a href="add_new.php">Add New Record</a></li>
-	</ul>
--- k/admin/admin/includes/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","root","","myweb") or
- die('Could not connect to the database!');
-
-
-// mysqli_select_db($con,) or
-//  die('No database selected!');
-?>
